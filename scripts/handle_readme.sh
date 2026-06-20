3f6d8b5331bb82ee767da1f689b61b202a387d29
olankens <olankens@users.noreply.github.com>
2026-06-20 11:59:30 +0200

    Employ the thumbnail prefix for assets

--- scripts/handle_readme.sh
+++ scripts/handle_readme.sh
@@ -4,12 +4,12 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 RME="$DIR/../README.md"
 SRC="$DIR/../.assets"
 
-ALL=("$SRC"/thumb-node*.avif)
+ALL=("$SRC"/thumbnail-node*.avif)
 MAX=${#ALL[@]}
 TXT=""
 for NUM in "${!ALL[@]}"; do
 	ALN=$( [ "$NUM" -lt $(( MAX - (MAX % 3 == 0 ? 3 : MAX % 3) )) ] && echo ' align="center"' || echo '' )
-	TXT="${TXT}<a href=\"wallpapers/$(basename "${ALL[$NUM]}" | sed "s/^thumb-//")\"><img src=\".assets/$(basename "${ALL[$NUM]}")\"${ALN} width=\"33.3333333333%\"></a>"
+	TXT="${TXT}<a href=\"wallpapers/$(basename "${ALL[$NUM]}" | sed "s/^thumbnail-//")\"><img src=\".assets/$(basename "${ALL[$NUM]}")\"${ALN} width=\"33.3333333333%\"></a>"
 done
 awk -v BLK="<p>${TXT}</p>" '
 	/<!-- START_NODE -->/ { print; print BLK; skip=1; next }
@@ -17,12 +17,12 @@ awk -v BLK="<p>${TXT}</p>" '
 	!skip
 ' "$RME" > "$RME.tmp" && mv "$RME.tmp" "$RME"
 
-ALL=("$SRC"/thumb-python*.avif)
+ALL=("$SRC"/thumbnail-python*.avif)
 MAX=${#ALL[@]}
 TXT=""
 for NUM in "${!ALL[@]}"; do
 	ALN=$( [ "$NUM" -lt $(( MAX - (MAX % 3 == 0 ? 3 : MAX % 3) )) ] && echo ' align="center"' || echo '' )
-	TXT="${TXT}<a href=\"wallpapers/$(basename "${ALL[$NUM]}" | sed "s/^thumb-//")\"><img src=\".assets/$(basename "${ALL[$NUM]}")\"${ALN} width=\"33.3333333333%\"></a>"
+	TXT="${TXT}<a href=\"wallpapers/$(basename "${ALL[$NUM]}" | sed "s/^thumbnail-//")\"><img src=\".assets/$(basename "${ALL[$NUM]}")\"${ALN} width=\"33.3333333333%\"></a>"
 done
 awk -v BLK="<p>${TXT}</p>" '
 	/<!-- START_PYTHON -->/ { print; print BLK; skip=1; next }
--- scripts/handle_thumbnails.sh
+++ scripts/handle_thumbnails.sh
@@ -7,6 +7,6 @@ OUT="$DIR/../.assets"
 for IMG in "$SRC"/*.avif; do
 	[ -e "$IMG" ] || continue
 	BNM="$(basename "${IMG%.*}")"
-	[ -e "$OUT/thumb-${BNM}.avif" ] && continue
-	ffmpeg -i "$IMG" -vf scale=iw/4:ih/4 -frames:v 1 "$OUT/thumb-${BNM}.avif"
+	[ -e "$OUT/thumbnail-${BNM}.avif" ] && continue
+	ffmpeg -i "$IMG" -vf scale=iw/4:ih/4 -frames:v 1 "$OUT/thumbnail-${BNM}.avif"
 done
